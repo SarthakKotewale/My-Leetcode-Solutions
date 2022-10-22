da15a76da2622da0f29a2c4e8648ce8e6f6f1bb4
SarthakKotewale <sarthakk0808@gmail.com>
2022-10-22 10:25:44 +0530

    added

--- Arrays/28_trapping-rain-water.java
+++ Arrays/28_trapping-rain-water.java
@@ -40,3 +40,36 @@ class Solution {
         return totalwater;
     }
 }
+
+// Optimised - Faster than 99.76%
+class Solution {
+    public int trap(int[] height) {
+        
+        int n  = height.length;
+        int left = 0;
+        int right = n - 1;
+        int maxleft = 0;
+        int maxright = 0;
+        int res = 0;
+        
+        while(left <= right){
+            if(height[left] <= height[right]){
+                if(height[left] >= maxleft){
+                    maxleft = height[left];
+                }else{
+                    res += maxleft - height[left];
+                }
+                left++;
+            }
+            else{
+                if(height[right] >= maxright){
+                    maxright = height[right];
+                }else{
+                    res += maxright - height[right];
+                }
+                right--;
+            }
+        }
+        return res;
+    }
+}

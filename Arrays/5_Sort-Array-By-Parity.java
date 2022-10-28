52b480bd3b5f84d4a0b8acfcab06ac29f074b69d
SarthakKotewale <sarthakk0808@gmail.com>
2022-10-28 11:18:42 +0530

    approach 2 added

--- Arrays/5_Sort-Array-By-Parity.java
+++ Arrays/5_Sort-Array-By-Parity.java
@@ -18,3 +18,33 @@ class Solution {
         return nums;
     }
 }
+
+//Approach 2 with STACKS
+
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        
+        Stack<Integer> odd = new Stack<>();
+        Stack<Integer> even = new Stack<>();
+        
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] % 2 != 0){
+                odd.push(nums[i]);
+            }else{
+                even.push(nums[i]);
+            }
+        }
+        
+        int[] ans = new int[nums.length];
+        int i = 0;
+        while(!even.isEmpty()){
+            ans[i] = even.pop();
+            i++;
+        }
+        while(!odd.isEmpty()){
+            ans[i] = odd.pop();
+            i++;
+        }
+        return ans;
+    }
+}

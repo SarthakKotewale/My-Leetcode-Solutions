4d70b55c385d8d84e20895e0e26847d9e320d4f4
SarthakKotewale <sarthakk0808@gmail.com>
2023-04-12 19:14:11 +0530

    Max Path Sum Root To Node Approach 2

--- Binary Trees/MaxPathSumRootToNode.java
+++ Binary Trees/MaxPathSumRootToNode.java
@@ -1,5 +1,5 @@
 class Solution {
-    
+
     static int max = Integer.MIN_VALUE;
     public static int maxPathSum(Node root){
         max = Integer.MIN_VALUE;
@@ -19,4 +19,33 @@ class Solution {
         travel(node.left, sum + node.data);
         travel(node.right, sum + node.data);
     }   
+}
+
+
+// Approach 2 
+
+class Solution {
+    public static int maxPathSum(Node root){
+
+        if(root.left != null && root.right != null){
+            int left = maxPathSum(root.left);
+            int leftd = Math.max(0, left); // if -ve don't add
+            int right = maxPathSum(root.right);
+            int rightd = Math.max(0, right);
+            return Math.max(leftd, rightd) + root.data;
+        }
+        else if(root.left != null){
+            int left = maxPathSum(root.left);
+            int leftd = Math.max(0, left);
+            return leftd  + root.data;
+        }
+        else if(root.right != null){
+            int right = maxPathSum(root.right);
+            int rightd = Math.max(0, right);
+            return rightd + root.data;
+        }
+        else{
+            return root.data;
+        }
+    }
 }

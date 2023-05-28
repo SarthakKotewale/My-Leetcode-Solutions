927816149342f104c18a17f4736f9f3431b0677b
SarthakKotewale <sarthakk0808@gmail.com>
2023-05-28 18:54:12 +0530

    Check AP(gfg) Method-2

--- Hashmaps & Heaps/14_check-arithmetic-progression(gfg).java
+++ Hashmaps & Heaps/14_check-arithmetic-progression(gfg).java
@@ -18,7 +18,7 @@ arr[] = {12, 40, 11, 20}
 Output: NO */
 
 
-//Method 1(By sorting)
+//Method 1(By sorting) -> 0(NlogN)
 class Solution {
     boolean checkIsAP(int arr[] ,int n){
         Arrays.sort(arr);
@@ -33,3 +33,35 @@ class Solution {
     }
 }
 
+
+//Method 2 -> 0(N)
+class Solution {
+    boolean checkIsAP(int arr[] ,int n){
+        //Method 2
+        
+        int min = Integer.MAX_VALUE;
+        int smin = Integer.MAX_VALUE;
+        
+        HashSet<Integer> set = new HashSet<>();
+        for(int val : arr){
+            set.add(val);
+            if(val < min){
+                smin = min;
+                min = val;
+            }else if(val < smin){
+                smin = val;
+            }
+        }
+        
+        int diff = smin - min;
+        for(int i = 0; i < n; i++){
+            // int term = min + (i - 1) * diff;
+            if(set.contains(min)){
+                min = min + diff;
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+}

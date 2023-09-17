7aeea7945e2d1c42e171a60c4f1ccac59e2a48e9
SarthakKotewale <sarthakk0808@gmail.com>
2023-09-17 18:17:56 +0530

    optimal soln added

--- Arrays/81_Sort-012.java
+++ Arrays/81_Sort-012.java
@@ -26,3 +26,28 @@ class Solution {
 }
 
 //Optimal
+class Solution {
+    public void sortColors(int[] arr) {
+        int low = 0;
+        int mid = 0;
+        int high = arr.length - 1;
+
+        while(mid <= high){
+            if(arr[mid] == 0){
+                swap(arr, low, mid);
+                low++;
+                mid++;
+            }else if(arr[mid] == 1){
+                mid++;
+            }else{
+                swap(arr, mid, high);
+                high--;
+            }
+        }
+    }
+    public void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}

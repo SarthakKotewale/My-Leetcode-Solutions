798f3782ce374eb559ced125b1bc1637a84ee419
SarthakKotewale <sarthakk0808@gmail.com>
2023-09-29 16:47:02 +0530

    LCS better code

--- Arrays/82_Longest-consecutive-sequence.java
+++ Arrays/82_Longest-consecutive-sequence.java
@@ -2,10 +2,10 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
         int longestSeq = 0;
-        for(int i = 0; i < nums.length; i++){
+        for (int i = 0; i < nums.length; i++) {
             int x = nums[i];
             int cnt = 1;
-            while(ls(nums, x + 1) == true){
+            while (ls(nums, x + 1) == true) {
                 cnt++;
                 x++;
             }
@@ -13,13 +13,38 @@ class Solution {
         }
         return longestSeq;
     }
-    public boolean ls(int[] nums, int x){
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] == x){
+
+    public boolean ls(int[] nums, int x) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == x) {
                 return true;
             }
         }
         return false;
     }
 }
+
 // Better
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        //Better
+        if(nums.length == 0) return 0;
+        
+        Arrays.sort(nums);
+        int lastSmaller = Integer.MIN_VALUE;
+        int cnt = 0;
+        int longestSeq = 1;
+
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] - 1 == lastSmaller){
+                cnt++;
+                lastSmaller = nums[i];
+            }else if(nums[i] != lastSmaller){
+                cnt = 1;
+                lastSmaller = nums[i];
+            }
+            longestSeq = Math.max(longestSeq, cnt);
+        }
+        return longestSeq;
+    }
+}

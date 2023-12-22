abe4284166dc1144446e781178e7d070c8602b1f
SarthakKotewale <sarthakk0808@gmail.com>
2023-12-22 10:25:43 +0530

    added gfg solution

--- Arrays/8_Product-of-Array-except-self.java
+++ Arrays/8_Product-of-Array-except-self.java
@@ -40,3 +40,26 @@ class Solution {
         return left;
     }
 }
+
+
+//gfg
+class Solution { 
+	public static long[] productExceptSelf(int nums[], int n) {
+	    long[] prefix = new long[nums.length];
+	    prefix[0] = 1;
+	    for(int i = 1; i < nums.length; i++){
+	        prefix[i] = prefix[i - 1] * nums[i - 1];
+	    }
+	    long[] suffix = new long[nums.length];
+	    suffix[nums.length - 1] = 1;
+	    for(int i = nums.length - 2; i >= 0; i--){
+	        suffix[i] = suffix[i + 1] * nums[i + 1];    
+	    }
+	    
+	    long[] ans = new long[nums.length];
+	    for(int i = 0; i < nums.length; i++){
+	        ans[i] = prefix[i] * suffix[i];
+	    }
+	    return ans;
+	} 
+} 
